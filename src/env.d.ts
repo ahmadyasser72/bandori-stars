6b82f8f7e3525f8ccbba46c3d2e8e8c28034697c
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-04 07:58:15 +0800

    if filtering by band, only suggest characters from that band

--- src/env.d.ts
+++ src/env.d.ts
@@ -11,6 +11,13 @@ declare global {
 	interface Window {
 		applyFormValueToSiblings: UnpolyUtilities["applyFormValueToSiblings"];
 	}
+
+	interface ObjectConstructor {
+		entries<T extends object>(o: T): Array<[keyof T, T[keyof T]]>;
+		keys<T extends object>(o: T): Array<keyof T>;
+
+		fromEntries<K extends string, V>(e: [K, V][]): Record<K, V>;
+	}
 }
 
 export {};
