ebdc255afa5e0523fb921758750117439c4ba2db
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-04 11:31:45 +0800

    persist selected filters in Astro session

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,22 +2,12 @@ type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals;
 
 declare namespace App {
 	interface Locals extends NetlifyLocals {}
-	interface SessionData {}
-}
-
-type UnpolyUtilities = typeof import("~/lib/unpoly");
-
-declare global {
-	interface Window {
-		applyFormValueToSiblings: UnpolyUtilities["applyFormValueToSiblings"];
-	}
-
-	interface ObjectConstructor {
-		entries<T extends object>(o: T): Array<[keyof T, T[keyof T]]>;
-		keys<T extends object>(o: T): Array<keyof T>;
-
-		fromEntries<K extends string, V>(e: [K, V][]): Record<K, V>;
+	interface SessionData {
+		card_filters: Partial<
+			Record<
+				(typeof import("~/lib/search/card"))["filterList"][number]["props"]["name"],
+				string
+			>
+		>;
 	}
 }
-
-export {};
