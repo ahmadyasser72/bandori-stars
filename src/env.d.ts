6aef1b2d699141058e9ff33e63f2a196328c75e6
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-04 13:22:13 +0800

    also persist card search options

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,11 +3,9 @@ type NetlifyLocals = import("@astrojs/netlify").NetlifyLocals;
 declare namespace App {
 	interface Locals extends NetlifyLocals {}
 	interface SessionData {
-		card_filters: Partial<
-			Record<
-				(typeof import("~/lib/search/card"))["filterList"][number]["props"]["name"],
-				string
-			>
+		card_search_filters: Partial<Record<"band" | "type" | "attribute", string>>;
+		card_search_options: Partial<
+			Record<"oldest_first" | "show_trained", string>
 		>;
 	}
 }
