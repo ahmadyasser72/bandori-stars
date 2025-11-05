a39d06da43b657f3cf4a988d98f120f9a32b6469
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-05 22:21:42 +0800

    cleanup stuffs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 		server: { hmr: false },
+		ssr: { external: ["node:fs", "node:path", "sharp"] },
 
 		build: {
 			rollupOptions: {
