ba6822535ca2f8916de18c0a22332849da263a8e
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-05 20:57:14 +0800

    try embedding js directly

---
+++ .contents/data.d.ts
@@ -0,0 +1,15 @@
+import type { ContentData } from "@/scripts/generate-content";
+
+export const {
+	band_map,
+	character_map,
+	card_map,
+	gacha_map,
+	event_map,
+}: ContentData;
+
+type MapValue<T extends Map<any, any>> =
+	T extends Map<any, infer Value> ? Value : never;
+
+type Collections = keyof ContentData;
+export type Entry<T extends Collections> = MapValue<ContentData[T]>;
--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import * as devalue from "devalue";
 
 import { dayjs } from "~/lib/date";
+import type { ContentData } from "./generate-content";
 
 export const toMap = <T extends { id: string } & object>(
 	array: T[],
@@ -11,13 +12,18 @@ export const toMap = <T extends { id: string } & object>(
 	new Map(array.map((data) => [data.id, data] as [string, T]));
 
 const BASE_CONTENT_PATH = path.resolve(__dirname, "../.contents/");
-export const save = async (data: any) => {
+export const save = async (data: ContentData) => {
 	await mkdir(BASE_CONTENT_PATH).catch(() => {});
 
-	const content = devalue.stringify(data, {
-		dayjs: (value) => dayjs.isDayjs(value) && value.unix(),
+	const content = devalue.uneval(data, (value) => {
+		if (dayjs.isDayjs(value)) {
+			return `dayjs.unix(${value.unix()})`;
+		}
 	});
-	await writeFile(path.join(BASE_CONTENT_PATH, "data.json"), content);
+	await writeFile(
+		path.join(BASE_CONTENT_PATH, "data.js"),
+		`import { dayjs } from "~/lib/date";export const {${Object.keys(data).join(",")}} = ${content};`,
+	);
 };
 
 export const timed = async <T>(
