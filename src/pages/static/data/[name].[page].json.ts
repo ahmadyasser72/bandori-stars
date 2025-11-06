47ea62e73663730aac8aeadb6ad1762760744056
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-06 10:52:56 +0800

    paginate json list data

--- src/pages/static/data/[name].[page].json.ts
+++ src/pages/static/data/[name].[page].json.ts
@@ -12,13 +12,21 @@ import { toJsonResponse } from "~/lib/utilities";
 export const prerender = true;
 
 export const GET: APIRoute<Props, Params> = ({ props }) =>
-	toJsonResponse({ keys: props.keys });
+	toJsonResponse({
+		items: props.page.data,
+		total: props.page.total,
+		page: props.page.currentPage,
+		prev: props.page.url.prev,
+		next: props.page.url.next,
+	});
 
-export const getStaticPaths = (() =>
-	Object.entries(data).map(([key, data]) => ({
-		params: { name: key.split("_")[0] },
-		props: { keys: [...data.keys()] },
-	}))) satisfies GetStaticPaths;
+export const getStaticPaths = (({ paginate }) =>
+	Object.entries(data).flatMap(([key, map]) =>
+		paginate([...map.values()], {
+			pageSize: 100,
+			params: { name: key.split("_")[0] },
+		}),
+	)) satisfies GetStaticPaths;
 
 type Params = InferGetStaticParamsType<typeof getStaticPaths>;
 type Props = InferGetStaticPropsType<typeof getStaticPaths>;
