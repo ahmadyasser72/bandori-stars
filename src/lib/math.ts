c7c5592576d5c259a39e8aef32f0fba96cb28b40
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-09 07:28:04 +0800

    add thousand separator for number

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,1 +1,7 @@
 export const sum = (values: number[]) => values.reduce((a, b) => a + b, 0);
+
+const numberFormatter = new Intl.NumberFormat("de-DE", {
+	style: "decimal",
+	notation: "standard",
+});
+export const formatNumber = (value: number) => numberFormatter.format(value);
