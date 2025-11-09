1c45cbf8229ec169adcc0449fe82f53667bf0097
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-09 22:28:35 +0800

    add passive free stars (daily login/lives)

--- src/pages/calculate/gacha/[gacha_id]/_calculate.ts
+++ src/pages/calculate/gacha/[gacha_id]/_calculate.ts
@@ -1,4 +1,5 @@
 import type { Entry } from "@/contents/data";
+import { dayjs } from "~/lib/date";
 
 export const calculateEvents = (
 	events: (Entry<"event_map"> | null)[],
@@ -18,3 +19,30 @@ export const calculateEvents = (
 			story: options.read_stories ? data.storyRewards : 0,
 			data,
 		}));
+
+export const calculatePassive = ({
+	from,
+	to,
+}: Record<"from" | "to", dayjs.Dayjs>) => {
+	// source: https://bandori.fandom.com/wiki/BanG_Dream!_Girls_Band_Party!/Login_Bonus#Normal_Login_Bonus
+	const dailyLogin = (() => {
+		const days = to.diff(from, "days");
+
+		const weeks = Math.floor(days / 7);
+		const weeklyStars = weeks * 150;
+
+		const leftOverDays = days % 7;
+		let extra = 0;
+		if (leftOverDays >= 4) extra = 100;
+		else if (leftOverDays >= 2) extra = 50;
+
+		return { stars: weeklyStars + extra };
+	})();
+
+	const dailyLives = (() => {
+		const weeks = to.diff(from, "weeks");
+		return { starGachaTicket: weeks };
+	})();
+
+	return { dailyLogin, dailyLives };
+};
