8941f4f344fdc3f8bd556a2d5feea8614a3de908
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-09 20:19:04 +0800

    more explicit API response caching, don't cache 10 oldest event/gacha

--- src/lib/bestdori/index.ts
+++ src/lib/bestdori/index.ts
@@ -1,7 +1,10 @@
 import * as asset from "./asset";
 import { fetchBestdori } from "./client";
 
-export const bestdori = <T>(pathname: string): Promise<T> =>
-	fetchBestdori(pathname).then((response) => response.json());
+export const bestdori = <T>(
+	pathname: string,
+	skipCache?: boolean,
+): Promise<T> =>
+	fetchBestdori(pathname, skipCache).then((response) => response.json());
 
 bestdori.asset = asset;
