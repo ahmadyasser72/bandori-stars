027a7d65a42003d6e895f345b7711eb64d4dc15b
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-09 13:07:22 +0800

    use findLast instead of reversing the array

--- src/pages/calculate/gacha/[gacha_id]/_calculate.ts
+++ src/pages/calculate/gacha/[gacha_id]/_calculate.ts
@@ -4,10 +4,8 @@ import type { Params } from "./_params";
 export const calculateEvents = (events: Entry<"event_map">[], params: Params) =>
 	events.map((data) => ({
 		point:
-			data.pointRewards
-				.slice()
-				.reverse()
-				.find(({ point }) => point <= params.point)?.stars ?? 0,
+			data.pointRewards.findLast(({ point }) => point <= params.point)?.stars ??
+			0,
 		rank:
 			data.rankingRewards.find(({ rank }) => rank >= params.rank)?.stars ?? 0,
 		story: params.readStories ? data.storyRewards : 0,
