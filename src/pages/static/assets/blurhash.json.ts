2c4c5d1eec6294d14cdc94fe16979f9f4d7bc57b
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-12 20:56:01 +0800

    sort blurhash.json entries

--- src/pages/static/assets/blurhash.json.ts
+++ src/pages/static/assets/blurhash.json.ts
@@ -10,26 +10,41 @@ import * as gacha from "./gacha/[id]_banner.[ext]";
 export const prerender = true;
 
 export const GET: APIRoute = async () => {
-	const assets = await Promise.all([
-		...card
-			.getStaticPaths()
-			.map(async ({ params, props }) => [
-				`/static/assets/card/${params.id}_${params.type}.${params.ext}`,
-				await bestdori.asset.card({ ...props, blurhash: true }),
-			]),
-		...event
-			.getStaticPaths()
-			.map(async ({ params, props }) => [
-				`/static/assets/event/${params.id}_${params.type}.${params.ext}`,
-				await bestdori.asset.event({ ...props, blurhash: true }),
-			]),
-		...gacha
-			.getStaticPaths()
-			.map(async ({ params, props }) => [
-				`/static/assets/gacha/${params.id}_banner.${params.ext}`,
-				await bestdori.asset.gachaBanner({ ...props, blurhash: true }),
-			]),
-	]).then(shuffle);
+	const entries = await Promise.all(
+		shuffle([
+			...card
+				.getStaticPaths()
+				.map(
+					async ({ params, props }) =>
+						[
+							`/static/assets/card/${params.id}_${params.type}.${params.ext}`,
+							await bestdori.asset.card({ ...props, blurhash: true }),
+						] as [string, string],
+				),
+			...event
+				.getStaticPaths()
+				.map(
+					async ({ params, props }) =>
+						[
+							`/static/assets/event/${params.id}_${params.type}.${params.ext}`,
+							await bestdori.asset.event({ ...props, blurhash: true }),
+						] as [string, string],
+				),
+			...gacha
+				.getStaticPaths()
+				.map(
+					async ({ params, props }) =>
+						[
+							`/static/assets/gacha/${params.id}_banner.${params.ext}`,
+							await bestdori.asset.gachaBanner({ ...props, blurhash: true }),
+						] as [string, string],
+				),
+		]),
+	);
 
-	return new Response(JSON.stringify(Object.fromEntries(assets)));
+	return new Response(
+		JSON.stringify(
+			Object.fromEntries(entries.sort(([a], [b]) => a.localeCompare(b))),
+		),
+	);
 };
