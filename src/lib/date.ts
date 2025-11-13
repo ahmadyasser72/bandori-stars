7c1c0b4db21b52b310cb097bbd3070e124bf24a6
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-13 18:48:39 +0800

    show predicted start date

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,9 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
 import isBetween from "dayjs/plugin/isBetween";
 import relativeTime from "dayjs/plugin/relativeTime";
 
+dayjs.extend(duration);
 dayjs.extend(isBetween);
 dayjs.extend(relativeTime);
 
