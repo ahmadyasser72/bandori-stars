cad3c681bd3a626fb921720c60396e5771f836f7
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-16 07:37:47 +0800

    directly use the release gap constant

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -3,22 +3,9 @@ import duration from "dayjs/plugin/duration";
 import isBetween from "dayjs/plugin/isBetween";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import { LATEST_EVENT_RELEASE_GAP } from "@/contents/data";
-
 dayjs.extend(duration);
 dayjs.extend(isBetween);
 dayjs.extend(relativeTime);
 
 export const now = () => dayjs();
 export { dayjs };
-
-export const getDateOffset = ({
-	en,
-	jp,
-}: {
-	en: dayjs.Dayjs | null;
-	jp: dayjs.Dayjs;
-}) => {
-	if (!en) return LATEST_EVENT_RELEASE_GAP;
-	return en.diff(jp);
-};
