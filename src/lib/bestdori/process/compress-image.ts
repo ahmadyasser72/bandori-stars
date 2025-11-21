9c455876262c3a72dd4a2d7a6dec3288373e7baf
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2025-11-21 20:29:09 +0800

    lazy load sharp again

--- src/lib/bestdori/process/compress-image.ts
+++ src/lib/bestdori/process/compress-image.ts
@@ -1,8 +1,6 @@
 import { existsSync, readFileSync } from "node:fs";
 import { join as joinPath } from "node:path";
 
-import sharp from "sharp";
-
 import {
 	BESTDORI_CACHE_DIR,
 	IMAGE_FORMAT,
@@ -20,6 +18,7 @@ export async function compressImage(
 ): Promise<Buffer | undefined> {
 	const path = getPath(name);
 	if (buffer && !existsSync(path)) {
+		const { default: sharp } = await import("sharp");
 		await sharp(buffer)
 			.resize({
 				width: MAX_IMAGE_WIDTH,
